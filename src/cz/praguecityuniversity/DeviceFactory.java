b403ee53d109fa76d59f00e13ec3180914df8d0a
AniSanikidze <56120787+AniSanikidze@users.noreply.github.com>
2022-01-25 01:13:04 +0400

    Implement IP Protocol

--- src/cz/praguecityuniversity/DeviceFactory.java
+++ src/cz/praguecityuniversity/DeviceFactory.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 public class DeviceFactory {
     public static Device getDevice(TypeofEntity typeofEntity, String deviceName, String macAddress) throws NotImplemented {
         return switch (typeofEntity) {
-            case COMPUTER -> new Device(deviceName, typeofEntity, macAddress, new IPProtocol(), null);
-            case ROUTER -> new Device(deviceName, typeofEntity, macAddress, new IPProtocol(), new HashMap<IPAddress, Integer>());
-            case SWITCH -> new Device(deviceName, typeofEntity, macAddress, null, null);
+            case COMPUTER, ROUTER -> new Device(deviceName, typeofEntity, macAddress, new IPProtocol(typeofEntity));
+            case SWITCH -> new Device(deviceName, typeofEntity, macAddress, null);
             default -> throw new NotImplemented("Implemented only for Router, Switch and Computer");
         };
     }
